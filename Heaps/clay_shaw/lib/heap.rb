90a51984106354940d9068270a9a34cbeb3fcd72
Clay Shaw <Claydshaw@gmail.com>
2017-10-19 13:20:39 -0700

    merge conflicts

--- Heaps/clay_shaw/lib/heap.rb
+++ Heaps/clay_shaw/lib/heap.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class BinaryMinHeap
   attr_reader :store, :prc
 
@@ -7,15 +9,25 @@ class BinaryMinHeap
   end
 
   def count
+    @store.length
   end
 
   def extract
+    @store[0], @store[-1] = @store[-1], @store[0]
+    # debugger
+    val = @store.pop
+    p @store
+    @store = self.class.heapify_down(@store, 0, @store.length, &prc)
+    return val
   end
 
   def peek
+    @store[0]
   end
 
   def push(val)
+    @store << val
+    @store = BinaryMinHeap.heapify_up(@store, @store.length - 1, @store.length, &@prc)
   end
 
   public
@@ -23,7 +35,7 @@ class BinaryMinHeap
   def self.child_indices(len, parent_index)
     # ex: (6, 0) => [1, 2]
     #     (6, 1) => [3, 4]
-    return [1, 2] if parent_index == 0
+    # return [1, 2] if parent_index == 0
     children = []
     left = (parent_index * 2) + 1
     right = (parent_index * 2) + 2
@@ -34,6 +46,7 @@ class BinaryMinHeap
     if right < len
       children << right
     end
+    p children
     return children
   end
 
@@ -51,21 +64,60 @@ class BinaryMinHeap
 # the node until it has reached the correct position
 # (aka neither of its children are greater).
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
-    idx = 0
-    # need to think about where to put the prc
+    prc ||= Proc.new { |a, b| a <=> b }
+    idx = parent_idx
+
     while idx < len
-      child_indices = self.child_indices(len, idx)
-      if @store[idx] > child_indices[0]
-        @store[idx], @store[child_indices] = @store[child_indices], @store[idx]
+      children = child_indices(len, idx)
+      debugger
+
+      # set the child we will pick first
+      next_child_idx = nil
+      if children.empty?
+        return array
+      elsif children.length == 1
+        next_child_idx = children[0]
+      elsif children.length == 2
+        # debugger
+        # if array[children[0]] && array[children[1]]
+        if children[0] && children[1]
+          if prc.call(array[children[0]], array[children[1]]) < 1
+            next_child_idx = children[0]
+          else
+            next_child_idx = children[1]
+          end
+        end
       end
-      if @store[idx] > child_indices[1]
-        @store[idx], @store[child_indices] = @store[child_indices], @store[idx]
+
+      # heapify
+      if array[next_child_idx] && prc.call(array[idx], array[next_child_idx]) == 1
+        array[idx], array[next_child_idx] = array[next_child_idx], array[idx]
       end
-      idx += 1
+      idx = next_child_idx
     end
+
+    array
   end
 
+# Next, implement ::heapify_up. This method will be used when adding
+# a new element to the heap to make sure that it is in the correct position.
+# It should take an array, a child index, and a length. Check the child
+# against its parent, and swap the elements if the parent is greater.
+# Continue until the node has reached the correct position.
   def self.heapify_up(array, child_idx, len = array.length, &prc)
+    return array if child_idx == 0
+    prc ||= Proc.new { |a, b| a <=> b }
+
+    parent_idx = parent_index(child_idx)
+    child_val = array[child_idx]
+    parent_val = array[parent_idx]
+    # debugger
+    if prc.call(array[child_idx], array[parent_idx]) >= 0
+       return array
+     else
+      array[child_idx], array[parent_idx] = parent_val, child_val
+      heapify_up(array, parent_idx, len, &prc)
+    end
 
   end
 end
