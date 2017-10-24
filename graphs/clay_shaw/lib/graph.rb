4b5326ae944e05b301f97de90c89c3ea6aac54b3
Clay Shaw <Claydshaw@gmail.com>
2017-10-24 09:08:30 -0700

    complete topo sort

--- graphs/clay_shaw/lib/graph.rb
+++ graphs/clay_shaw/lib/graph.rb
@@ -1,12 +1,16 @@
 class Vertex
-  attr_accessor :value, :in_edge, :out_edge
-  
+  attr_accessor :value, :in_edges, :out_edges
+
   def initialize(value)
   # typically reppd by dots
   # we pick one or the other, @in_edge or @out_edge depending on how we implement
     @value = value
-    @in_edge = []
-    @out_edge = []
+    @in_edges = []
+    @out_edges = []
+  end
+
+  def in_count
+    @in_edges.length
   end
 end
 
@@ -17,12 +21,18 @@ class Edge
     @from_vertex = from_vertex
     @to_vertex = to_vertex
     @cost = cost
+
+    to_vertex.in_edges << self
+    from_vertex.out_edges << self
   end
 
   # set edge should also be in graph class for easier accessibility
 
   def destroy!
-
+    @to_vertex.in_edges.delete(self)
+    @from_vertex.out_edges.delete(self)
+    @from_vertex = nil
+    @to_vertex = nil
   end
 end
 
@@ -30,8 +40,8 @@ end
 # 1. Init a queue
 # 2. push in all vertices that have no edges
 # 3. when complete, pop out all vertices and examine
-# 4. place current vertices in a sorted array. 
-# 5. As we pop we need to see all OUT edges on each element as we pop 
+# 4. place current vertices in a sorted array.
+# 5. As we pop we need to see all OUT edges on each element as we pop
 # 6. if destination has no more (IN) edges, push that element onto the queue
 # 7 push that into our results array
 # 8. continue until there is nothing left in our queue
--- graphs/clay_shaw/lib/topological_sort.rb
+++ graphs/clay_shaw/lib/topological_sort.rb
@@ -1,7 +1,52 @@
 require_relative 'graph'
-
+require 'byebug'
 # Implementing topological sort using both Khan's and Tarian's algorithms
+# non-deterministic
 
+# sparse graphs O(n)
+# dense graphs O(n**2)
 def topological_sort(vertices)
+  sorted = []
+  top = [] #queue
+  # init a graph
+  in_degrees = Hash.new
 
+
+  # step 1 O(|V|)
+  vertices.each do |vertex|
+    top.push(vertex) if vertex.in_edges.empty?
+
+    in_degrees[vertex] = vertex.in_count
+
+  end
+
+  # put every node/vertex in and then out
+  # O(|E|) all edge sets for second loop
+  until top.empty?
+    current = top.shift
+    sorted << current
+
+    current.out_edges.each do |edge|
+      destination = edge.to_vertex
+      # p destination
+
+      # take edge out of the graph
+      in_degrees[destination] -= 1
+
+
+
+      if in_degrees[destination] == 0
+        top.push(destination)
+        # in_degrees[destination] += 1
+      end
+    end
+    # remove vertex from graph
+    # in_degrees.delete(current)
+  end
+
+  if sorted.length != vertices.length
+    return []
+  else
+    return sorted
+  end
 end
