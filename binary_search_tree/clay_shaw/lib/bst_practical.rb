89b823ba459cbd81cb8f8600eb2d363a19e33e53
Clay Shaw <Claydshaw@gmail.com>
2017-11-09 11:25:20 -0800

    add comments

--- binary_search_tree/clay_shaw/lib/bst_practical.rb
+++ binary_search_tree/clay_shaw/lib/bst_practical.rb
@@ -8,13 +8,16 @@ end
 def reverse_inorder_traversal(tree_node, kth_node, k)
   p kth_node
   if tree_node && kth_node[:count] < k
+    # traverse right side (this side first since we will be looking for largest)
     reverse_inorder_traversal(tree_node.right, kth_node, k)
 
+    # increment counter if we still havent hit k
     if kth_node[:count] < k
       kth_node[:count] += 1
       kth_node[:correct_node] = tree_node
     end
   
+    # traverse left child to get to its "rights"
     if kth_node[:count] < k
       reverse_inorder_traversal(tree_node.left, kth_node, k)
     end
