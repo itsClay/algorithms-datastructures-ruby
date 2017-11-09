5a1f7b5e92aec051c092c35fb7423c9de0a09784
Clay Shaw <Claydshaw@gmail.com>
2017-11-09 11:23:19 -0800

    update with solution to kth largest

--- binary_search_tree/clay_shaw/lib/bst_practical.rb
+++ binary_search_tree/clay_shaw/lib/bst_practical.rb
@@ -1,3 +1,24 @@
+require 'byebug'
 
 def kth_largest(tree_node, k)
+  kth_node = {count: 0, correct_node: nil}
+  reverse_inorder_traversal(tree_node, kth_node, k)[:correct_node]
+end
+
+def reverse_inorder_traversal(tree_node, kth_node, k)
+  p kth_node
+  if tree_node && kth_node[:count] < k
+    reverse_inorder_traversal(tree_node.right, kth_node, k)
+
+    if kth_node[:count] < k
+      kth_node[:count] += 1
+      kth_node[:correct_node] = tree_node
+    end
+  
+    if kth_node[:count] < k
+      reverse_inorder_traversal(tree_node.left, kth_node, k)
+    end
+  end
+
+  kth_node
 end
