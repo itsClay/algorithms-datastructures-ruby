d9a423019e09c5758a5956441a1f589e17c36e16
Clay Shaw <Claydshaw@gmail.com>
2017-11-10 10:31:06 -0800

    implement abstract idea of preorder and postorder

--- binary_search_tree/solutions/lib/interview_questions.rb
+++ binary_search_tree/solutions/lib/interview_questions.rb
@@ -1,7 +1,36 @@
 # Post Order Traversal:
+# each node is visited AFTER its subtrees. We can do left to right or right 
+# to left.
+def post_order_traversal(tree_node)
+  if tree_node.left
+    post_order_traversal(tree_node.left)
+  end
 
+  if tree_node.right
+    post_order_traversal(tree_node.right)
+  end
+
+  p tree_node
+end
 
 # Pre Order Traversal:
+# each node is visited BEFORE its subtrees. We can do left to right or right 
+# to left.
+# 1. Visit the root node (normally output it)
+# 2. Do a pre order traversal of the left subtree
+# 3. Do a pre-order traversal of the right subtree
+def pre_order_traversal(tree_node)
+  # visit root node
+  p tree_node
+
+  if tree_node.left
+    pre_order_traversal(tree_node.left)
+  end
+
+  if tree_node.right
+    pre_order_traversal(tree_node.right)
+  end
+end
 
 
 # LCA: 
@@ -12,6 +41,4 @@
 # tree shares at least one ancestor -- the root. In this exercise, write a 
 # function that takes in a BST and two nodes, and returns the node that is the 
 # lowest common ancestor of the given nodes. Assume no duplicate values.
-
-
 
